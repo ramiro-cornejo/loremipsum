48643365670ce897eb3e9b2c1b2368060a8c5b6f
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2023-07-13 12:11:35 -0300

    Corregir clase03

--- angular/clase03/appclase03/src/app/app.module.ts
+++ angular/clase03/appclase03/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,9 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule
+  ],
+  exports: [
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
